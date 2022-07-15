76325fe328514549e0cd84bd4d08358a44617374
cmlowerence123 <101648817+cmlowerence123@users.noreply.github.com>
2022-07-15 13:53:31 +0530

    Updates

---
+++ precache-manifest.f377cb52e788a3727d50feeb140553be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b397bdb6f0cc9533192",
+    "url": "/textutils/static/css/main.420f4168.chunk.css"
+  },
+  {
+    "revision": "3b397bdb6f0cc9533192",
+    "url": "/textutils/static/js/main.3b397bdb.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textutils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8c5d3a7ccc996c2dc493",
+    "url": "/textutils/static/js/runtime~main.8c5d3a7c.js"
+  },
+  {
+    "revision": "594bd7f8ccd30ff474df43d0727f6bdd",
+    "url": "/textutils/index.html"
+  }
+];
